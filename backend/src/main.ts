9827c7fb03cab32b58778d42a652a6c7d87083f2
Fatima_Iqra <iqraf683@gmail.com>
2026-06-14 11:51:53 +0500

    feat: add exception handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +14,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
+ app.useGlobalFilters(
+  new HttpExceptionFilter(),
+);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
