5e90a6eec4c40eceb363894d117eb7f850939573
Fatima_Iqra <iqraf683@gmail.com>
2026-06-14 10:42:31 +0500

    Apply Rate Limit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentModule } from './modules/document/document.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { RetrievalService } from './modules/chat/retrieval/retrieval.service';
 import { ChatModule } from './modules/chat/chat.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [DocumentModule, PrismaModule, ChatModule],
+  imports: [
+    DocumentModule,
+    PrismaModule,
+    ChatModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 120000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService, RetrievalService],
+  providers: [
+    AppService,
+    //global rate limit guard
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: ThrottlerGuard,
+    // },
+  ],
 })
 export class AppModule {}
--- backend/src/modules/chat/chat.controller.ts
+++ backend/src/modules/chat/chat.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { AskQuestionDto } from './dto/ask-question.dto';
+import { Throttle } from '@nestjs/throttler/dist/throttler.decorator';
 
 @Controller('chat')
 export class ChatController {
   constructor(
     private readonly chatService: ChatService,
   ) {}
-
+@Throttle({
+  default: {
+    limit: 10,
+    ttl: 120000,
+  },
+})
   @Post('ask')
   async ask(
     @Body() dto: AskQuestionDto,
